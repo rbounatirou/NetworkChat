3a3c35109480c8001b2dbc7098953e11d887213f
Angelo CONGIU <Angelo CONGIU@DESKTOP-822VRSS>
2024-07-21 22:03:37 +0200

    signalr test sur console

--- Code/Serveur/ServerApp/RemoteServer.cs
+++ Code/Serveur/ServerApp/RemoteServer.cs
@@ -4,14 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Remoting;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting.Channels.Tcp;
+
 
 namespace ServerApp
 {
     internal class RemoteServer
     {
-        TcpChannel chan = new TcpChannel(8085);
-        ChannelServices.RegisterChannel(chan);
     }
 }
---
+++ Code/WithSignalR/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace WithSignalR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
